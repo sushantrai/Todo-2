9090698de0ce70aaa9ffd2cca9220bc889012252
Sushant Kumar <mail.sushanttgmail.com>
2024-12-25 22:11:49 +0530

    Initial commit for Todo-2 project

---
+++ src/Components/TodoForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { useTodo } from "../Context";
+
+function Todoform() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+  const add = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({ id: Date.now(), todo: todo, completed: false });
+  };
+  return (
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default Todoform;
---
+++ src/Context/TodoContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useContext } from "react";
+export const TodoContext = createContext({
+  todos: [{ id: 1, todo: "Todo msg", completed: false }],
+  addTodo: () => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toogleComplete: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
